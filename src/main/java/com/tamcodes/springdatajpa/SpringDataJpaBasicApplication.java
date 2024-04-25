5b418b4452c18acf3e0c6296685e8aeba6f83100
Tamaghno Chaudhuri <tamaghno.chaudhuri@gmail.com>
2024-04-25 09:41:59 +0530

    #initial structure

---
+++ src/main/java/com/tamcodes/springdatajpa/SpringDataJpaBasicApplication.java
@@ -0,0 +1,13 @@
+package com.tamcodes.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaBasicApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tamcodes/springdatajpa/models/author.java
@@ -0,0 +1,58 @@
+package com.tamcodes.springdatajpa.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity //this is from Jakarta persistence
+@Table(name = "AUTHOR_TBL")
+public class author {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "author_sequence")
+    /*
+    (
+            strategy = GenerationType.TABLE,
+            generator = "generator_table")
+     */
+    @SequenceGenerator(
+            name = "author_sequence",
+            sequenceName = "author_sequence",
+            allocationSize = 1
+    )
+    /*
+    @TableGenerator(
+            name = "generator_table",
+            table = "generator_table",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
+            allocationSize = 1
+    )
+     */
+    private Integer id;
+
+    @Column(name = "FIRST_NAME", length = 15)
+    private String firstname;
+
+    private String lastname;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private int age;
+
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(insertable = false)
+    private LocalDateTime lastModified;
+}
